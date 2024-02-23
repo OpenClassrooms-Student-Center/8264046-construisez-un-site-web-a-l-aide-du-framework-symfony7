8e450cfe8ea76063d5f7dd9983ebd9da5b47536a
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2024-02-23 14:51:08 +0100

    Part1 - Chapter5 : Add static templates

---
+++ Part1_Chap5/biblios/assets/app.js
@@ -0,0 +1,13 @@
+import './bootstrap.js';
+import './js/scripts';
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * This file will be included onto the page via the importmap() Twig function,
+ * which should already be in your base.html.twig.
+ */
+import './vendor/bootstrap/dist/css/bootstrap.min.css';
+import './styles/styles.css';
+import './styles/app.css';
+
+console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
